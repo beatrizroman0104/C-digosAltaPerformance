6375bed0fa6d96c41afbafe30e581377cb92f428
Beatriz <beatrizgroman@gmail.com>
2026-03-16 11:19:45 -0300

    Update Aplicacao.java

--- src/CódigoAltaPerformance/Aplicacao.java
+++ src/CódigoAltaPerformance/Aplicacao.java
@@ -1,4 +1,10 @@
 package CódigoAltaPerformance;
 
 public class Aplicacao {
+  public static void main(String[] args){
+        Aluno a1 = new Aluno();
+        a1.leitura();
+        a1.calcularMedia();
+        a1.mostraAluno();
+    }
 }
